81697ca5454b1fedafe09f993e83d0929a5586e0
Priyank Rajput <priyanksingh8@gmail.com>
2019-02-07 16:19:56 +0530

    Built Burger Builder basic

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
-const layout = (props) => {
-    return (
-      <Aux>
-          <Toolbar/>
-          <main className={classes.Content}>
-              {props.children}
-          </main>
-      </Aux>
-    );
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component  {
+    state = {
+      showSideDrawer: false
+    };
+    sideDrawerClosedHandler = () => {
+        this.setState({
+            showSideDrawer: false
+        })
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer}
+        });
+    };
+    render() {
+        return (
+            <Aux>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
 };
 
-export default layout;
+export default Layout;
